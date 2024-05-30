44a6aab42a6b6f57adb6f401a983c8a52eaf9cbd
root <root@C-Burker>
2024-05-29 21:34:08 -0400

    update artwork

--- scripts/spray_bottle.gd
+++ scripts/spray_bottle.gd
@@ -1,8 +1,13 @@
 extends Node2D
 @onready var killzone = $AnimatedSprite2D/killzone
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var killzone_2 = $AnimatedSprite2D/killzone2
 
+var is_squashed = false
 
 func _on_waterbottle_weakspot_body_entered(body):
-	print("You stomped it alright")
-	killzone.queue_free()
-	self.queue_free()
+	if not is_squashed:
+		is_squashed = true
+		killzone.queue_free()
+		killzone_2.queue_free()
+		animated_sprite_2d.play('squash')
